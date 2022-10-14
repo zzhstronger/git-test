d48bd8047232b7c7f3460c4fdd06ffc4d963656e
zzhstronger <zzhstronger@zzh.com>
2022-10-14 12:30:17 +0800

    master second commit

--- src/main/java/com/zzh/GitTest.java
+++ src/main/java/com/zzh/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git!2");
         System.out.println("hello git!3");
         System.out.println("hello git!4");
+        System.out.println("master test");
     }
 }
