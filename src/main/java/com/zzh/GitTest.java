3114ddd34d82022acae6f06cbd28e8774da32d33
zzhstronger <zzhstronger@zzh.com>
2022-10-14 12:20:33 +0800

    hot-fix first commit

--- src/main/java/com/zzh/GitTest.java
+++ src/main/java/com/zzh/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git!2");
         System.out.println("hello git!3");
+        System.out.println("hello git!4");
     }
 }
