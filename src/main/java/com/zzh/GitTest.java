fb35d4c565a312a3f8bdde2455c4029e4dd48595
zzhstronger <zzhstronger@zzh.com>
2022-10-14 11:31:37 +0800

    second commit

--- src/main/java/com/zzh/GitTest.java
+++ src/main/java/com/zzh/GitTest.java
@@ -3,5 +3,6 @@ package com.zzh;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!2");
     }
 }
