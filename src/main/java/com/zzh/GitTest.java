a58050be233c38e1ab3ec0e37684330d1ca2995a
zzhstronger <zzhstronger@zzh.com>
2022-10-15 22:47:26 +0800

    push commit

--- src/main/java/com/zzh/GitTest.java
+++ src/main/java/com/zzh/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git!4");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test!");
     }
 }
