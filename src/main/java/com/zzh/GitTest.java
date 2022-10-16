4e174cb44a059d3e4446a216dc9182592be9d08f
zzhstronger <3595438729@qq.com>
2022-10-16 15:59:29 +0000

    update src/main/java/com/zzh/GitTest.java.
    Signed-off-by: zzhstronger <3595438729@qq.com>

--- src/main/java/com/zzh/GitTest.java
+++ src/main/java/com/zzh/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("push test!");
         System.out.println("pull test!");
         System.out.println("push test to Gitee!");
+        System.out.println("gitee test");
     }
 }
