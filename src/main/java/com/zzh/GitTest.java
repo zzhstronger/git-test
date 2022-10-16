59847e82668e513f9c9f6ab3ad222b26677c00f0
zzhstronger <zzhstronger@zzh.com>
2022-10-16 23:22:13 +0800

    first commit

--- src/main/java/com/zzh/GitTest.java
+++ src/main/java/com/zzh/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("push test!");
         System.out.println("pull test!");
+        System.out.println("push test to Gitee!");
     }
 }
